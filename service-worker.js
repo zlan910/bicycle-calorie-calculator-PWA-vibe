32886508c5e2b36b2ee58618c89d69a95ac4b4af
Zach Lancaster <zlancaster@undreamt.ai>
2026-07-13 17:00:49 -0500

    Show app version and source link

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-// Bump the minor version for each release; reserve major bumps for major releases.
-const APP_VERSION = "0.2";
+importScripts("./version.js");
 const CACHE_NAME = `bike-calories-v${APP_VERSION}`;
-const APP_FILES = ["./", "./index.html", "./styles.css", "./app.js", "./calculator.js", "./manifest.webmanifest", "./icons/bicycle_cals.svg", "./icons/bicycle_cals_192.png", "./icons/bicycle_cals_512.png"];
+const APP_FILES = ["./", "./index.html", "./styles.css", "./app.js", "./calculator.js", "./version.js", "./manifest.webmanifest", "./icons/bicycle_cals.svg", "./icons/bicycle_cals_192.png", "./icons/bicycle_cals_512.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
---
+++ version.js
@@ -0,0 +1,2 @@
+// Bump the minor version for each release; reserve major bumps for major releases.
+globalThis.APP_VERSION = "0.2";
